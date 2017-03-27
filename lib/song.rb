a17e2f58966d143d463312801be8dbb559cf343c
ethanryan <ethanryan@gmail.com>
2017-03-27 17:49:48 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,9 @@
 require_relative "./interactive_record.rb"
 
+# the methods we write that allow a given Ruby class
+# and instances to communicate with a database
+# are not specific to any one class
+
 class Song < InteractiveRecord
 
   self.column_names.each do |col_name|
